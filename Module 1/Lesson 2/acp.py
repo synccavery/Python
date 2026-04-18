d5a94e186fcd7fadb3a5e6c33059a185ce158d5a
synccavery <amichak.ac@gmail.com>
2026-04-18 20:43:06 +0600

    ................

---
+++ Module 1/Lesson 2/acp.py
@@ -0,0 +1,11 @@
+a = "21st May"
+b = "7th April"
+c = "28th March"
+d = "25th December"
+e = "21st April"
+
+print("A's Birthday: " + a)
+print("B's Birthday: " + b)
+print("C's Birthday: " + c)
+print("D's Birthday: " + d)
+print("E's Birthday: " + e)
