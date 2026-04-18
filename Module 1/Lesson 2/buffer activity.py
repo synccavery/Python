5cc54f8b30a50efc38d1d9c81814ca2604bababb
synccavery <amichak.ac@gmail.com>
2026-04-18 20:34:26 +0600

    ''''''''''''''''

---
+++ Module 1/Lesson 2/buffer activity.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+age = input("How old are you?")
+city = input("Which city do you live in?")
+
+print( "Your name: " + name)
+print( "Your age: " + age)
+print("Your city: " + city)
