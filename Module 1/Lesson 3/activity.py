495cf27876afccb1c8ca305dc7e0d03292d966cb
synccavery <amichak.ac@gmail.com>
2026-04-24 20:27:14 +0600

    .....................

---
+++ Module 1/Lesson 3/activity.py
@@ -0,0 +1,15 @@
+a = 5
+b = 24.3
+c = True
+d = "coding"
+print(f"Data type of {a} is ",type(a))
+print(f"Data type of {b} is ",type(b))
+print(f"Data type of {c} is ",type(c))
+print(f"Data type of {d} is ",type(d))
+#Typecasting
+b = int(b)
+print("Value of b:", b)
+print(f"Data type of {b} is ",type(b))
+c = str(c)
+print("Value of c:", c)
+print(f"Data type of {c} is ",type(c))
