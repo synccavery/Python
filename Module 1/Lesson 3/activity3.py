0c11b5c19f692c8731907fef2e88c5f272ddb9de
synccavery <amichak.ac@gmail.com>
2026-04-24 20:40:56 +0600

    ..............

---
+++ Module 1/Lesson 3/activity3.py
@@ -0,0 +1,14 @@
+word = input("Enter your word: ")
+#Length of the word
+print("Length: ", len(word))
+#Indexing
+print(word[3])
+#Slicing
+print(word[0:4])
+#Concatenation
+word2 = "coding"
+print(word+word2)
+#converting to upper case
+print(word.upper())
+#Reverse a String
+print(word[::-1])
