6ec6b5772fb77024df71554d88c7b110a5fa3c15
synccavery <amichak.ac@gmail.com>
2026-05-01 20:21:28 +0600

    ...............

---
+++ Module 1/lesson 5/activity2.py
@@ -0,0 +1,6 @@
+selling_price = float(input("Enter your selling price: "))
+buying_price = float(input("Enter your buying price: "))
+if selling_price>buying_price:
+    print("Profit")
+else:
+    print("Loss")
