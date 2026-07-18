4d596bef5951d564b3c88f6717bab48b00a44d59
synccavery <amichak.ac@gmail.com>
2026-07-18 20:24:58 +0600

    ..................

---
+++ Module 4/Lesson 1/activity1.py
@@ -0,0 +1,15 @@
+my_list1 = [1, 2, 3,"Python", 2.345, True, "Codingal" ]
+print(my_list1)
+#length of the list
+print(len(my_list1))
+#Accessing an item from the list
+print(my_list1[3])
+print(my_list1[6])
+#Slicing
+my_list2 = my_list1[3:6]
+print(my_list2)
+#Iterate through a loop
+for item in my_list1:
+    print(item)
+#Reversing a list
+print(my_list1[::-1])
