5219b52b470580538934e8bcf0da4edcd445f913
synccavery <amichak.ac@gmail.com>
2026-08-01 20:37:37 +0600

    ....................

--- Module 4/Lesson 3/activity.py
+++ Module 4/Lesson 3/activity.py
@@ -6,3 +6,13 @@ basket2.add("orange")
 print(f"Basket 2 after adding orange: {basket2}")
 common_fruits = basket1.intersection(basket2)
 print(f"Common fruits between 1&2: {common_fruits}")
+import array as arr
+fruit_counts = arr.array('i',[3,5,2,4])
+print(fruit_counts)
+
+fruit_counts.insert(0,1)
+print(fruit_counts)
+fruit_counts.append(6)
+print(fruit_counts)
+fruit_counts.reverse()
+print(fruit_counts)
